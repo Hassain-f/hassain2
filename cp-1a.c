c41b1355b873d6870278541f8de55f77983e7aa9
Hassain-f <hassainfazuluddin@gmail.com>
2024-12-10 15:23:52 +0530

    Add files via upload

---
+++ cp-1a.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+
+    float radius,diameter,circumference,area;
+    printf("enter radius of circle:");
+    scanf("%f",&radius);
+    diameter=2*radius;
+    circumference=2*3.14*radius;
+    area=3.14*(radius*radius);
+    printf("diameter of circle=%.2f units\n",diameter);
+    printf("circumference of circle=%.2f units\n",circumference);
+    printf("area of circle=%.2f units\n,area");
+    return 0;
+}
+
